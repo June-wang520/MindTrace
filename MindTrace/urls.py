e6be8f6751d20076fbeede97b4d2f07881706c84
Johnny <3533222@qq.com>
2025-11-02 05:31:58 -0800

    Add redirect from / to /entries/

--- MindTrace/urls.py
+++ MindTrace/urls.py
@@ -15,15 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.shortcuts import redirect
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('entries/', include('entries.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('', lambda request: redirect('entries/')),
 ]
 
 if settings.DEBUG:
