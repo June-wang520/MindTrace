08d6f81fc745e006b49aacea64df68b1c5db0b96
Johnny <3533222@qq.com>
2025-11-06 23:30:05 -0800

    Update latest changes

--- MindTrace/settings.py
+++ MindTrace/settings.py
@@ -76,11 +76,17 @@ WSGI_APPLICATION = 'MindTrace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'KKjl7ly48DOLaMUl',
+        'HOST': 'db.akgpcmttdbggyqeizqag.supabase.co',
+        'PORT': '5432',
     }
+
 }
 
 
